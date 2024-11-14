86bd8c1c0c4d78d4c9cfdb29c2dc5a5a36d4ae3b
SPOOKZ <53491846+ReadyP1ayerTwo@users.noreply.github.com>
2024-11-14 18:01:19 -0500

    Update config.lua

--- config.lua
+++ config.lua
@@ -19,6 +19,60 @@ Challenges_list = {
         ["end"] = "EndLongestFreefallSurvivedChallenge",
         ["countdown"] = 180000
     },
+    {
+        ["name"] = "longest_stoppie",
+        ["start"] = "StartLongestStoppieChallenge",
+        ["end"] = "EndLongestStoppieChallenge",
+        ["countdown"] = 180000
+    },
+    {
+        ["name"] = "longest_wheelie",
+        ["start"] = "StartLongestWheelieChallenge",
+        ["end"] = "EndLongestWheelieChallenge",
+        ["countdown"] = 180000
+    },
+    {
+        ["name"] = "inverted_flying",
+        ["start"] = "StartInvertedFlyingChallenge",
+        ["end"] = "EndInvertedFlyingChallenge",
+        ["countdown"] = 180000
+    },
+    {
+        ["name"] = "low_flying",
+        ["start"] = "StartLowFlyingChallenge",
+        ["end"] = "EndLowFlyingChallenge",
+        ["countdown"] = 180000
+    },
+    {
+        ["name"] = "chaos_challenge",
+        ["start"] = "StartChaosChallenge",
+        ["end"] = "EndChaosChallenge",
+        ["countdown"] = 180000
+    },
+    {
+        ["name"] = "farthest_jump",
+        ["start"] = "StartFarthestJumpChallenge",
+        ["end"] = "EndFarthestJumpChallenge",
+        ["countdown"] = 180000
+    },
+    {
+        ["name"] = "flying_under_bridges",
+        ["start"] = "StartFlyingUnderBridgesChallenge",
+        ["end"] = "EndFlyingUnderBridgesChallenge",
+        ["countdown"] = 180000
+    },
+    {
+        ["name"] = "near_miss",
+        ["start"] = "StartNearMissChallenge",
+        ["end"] = "EndNearMissChallenge",
+        ["countdown"] = 180000
+    },
+    {
+        ["name"] = "top_speed",
+        ["start"] = "StartTopSpeedChallenge",
+        ["end"] = "EndTopSpeedChallenge",
+        ["countdown"] = 180000
+    },
 }
 
 Config = {
