ddc04fed164ab93d922a9d84c40a51ed3e411e23
SPOOKZ <53491846+ReadyP1ayerTwo@users.noreply.github.com>
2024-11-17 07:31:44 -0500

    Functional farthest jump!

--- client/challenges/farthest_jump.lua
+++ client/challenges/farthest_jump.lua
@@ -1,37 +1,67 @@
 local farthestJumpDistance = 0.0
+local currentJumpDistance = 0.0
+local isJumping = false
+local jumpStartCoords = nil
 
 function StartFarthestJumpDistanceChallenge()
     InChallenge = true
     farthestJumpDistance = 0.0
+    currentJumpDistance = 0.0
+    isJumping = false
+    jumpStartCoords = nil
 
     RemoveAllPedWeapons(PlayerPedId(), true)
 
     StartMissionScreen("Farthest Jump Distance Challenge", "Find a land vehicle and prepare for the challenge.")
 
     Citizen.CreateThread(function()
-
         while InChallenge do
-            local _, currentJumpDistance = StatGetFloat(`MP0_LONGEST_JUMP_DISTANCE`, -1)
-            currentJumpDistance = math.round(currentJumpDistance, 2)
-            if currentJumpDistance > farthestJumpDistance then
-                farthestJumpDistance = currentJumpDistance
+            local vehicle = GetVehiclePedIsIn(PlayerPedId(), false)
+            if vehicle ~= 0 and IsVehicleOnAllWheels(vehicle) == false then
+                -- The player is in the air
+                if not isJumping then
+                    isJumping = true
+                    jumpStartCoords = GetEntityCoords(vehicle)
+                else
+                    -- Calculate current jump distance
+                    local currentCoords = GetEntityCoords(vehicle)
+                    currentJumpDistance = #(currentCoords - jumpStartCoords)
+                end
+            else
+                -- The player has landed
+                if isJumping then
+                    isJumping = false
+                    if currentJumpDistance > farthestJumpDistance then
+                        farthestJumpDistance = currentJumpDistance
+                    end
+                    currentJumpDistance = 0.0 -- Reset for the next jump
+                end
             end
-            Citizen.Wait(1000)
+            Citizen.Wait(100) -- Check every 100ms
         end
     end)
 
     Citizen.CreateThread(function()
         while InChallenge do
+            -- Display instructions to the player
             DrawInstruction("Find a land vehicle and achieve the farthest jump distance.")
-            TriggerServerEvent("mth-challenges:updateEvent", farthestJumpDistance)
-            Citizen.Wait(500)
+
+            -- Report rounded farthest jump distance to the server
+            local roundedDistance = math.floor(farthestJumpDistance * 100) / 100 -- Round to 2 decimal places
+            TriggerServerEvent("mth-challenges:updateEvent", roundedDistance)
+
+            Citizen.Wait(500) -- Update every 500ms
         end
     end)
 end
 
 function EndFarthestJumpDistanceChallenge()
     farthestJumpDistance = 0.0
+    currentJumpDistance = 0.0
+    isJumping = false
+    jumpStartCoords = nil
     InChallenge = false
+
     SetPlayerWantedLevel(PlayerId(), 0, false)
     SetPlayerWantedLevelNow(PlayerId(), false)
 end
